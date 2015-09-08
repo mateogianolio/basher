3c0f64a7cf67980b5e675811f671c2dd1ad3e783
Mateo Gianolio <gianoliomateo@gmail.com>
2015-09-09 00:54:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  var execSync = require('exec-sync');
+
+  // basher({ function_name: <code> })
+  module.exports = function(bindings) {
+    if(bindings === undefined)
+      throw new Error('invalid argument');
+
+    // e.g. basher(<code>)
+    if(typeof bindings === 'string')
+      return parse.bind(null, bindings);
+    else if(typeof bindings !== 'object')
+      throw new Error('unknown argument type');
+
+    var functions = function() {},
+        binding,
+        output;
+
+    for(var name in bindings)
+      functions[name] = parse.bind(null, bindings[name]);
+
+    return functions;
+  };
+
+  function parse(binding) {
+    var args = Array.prototype.slice.call(arguments, 0),
+        i = 1;
+    while(binding.indexOf('{' + i + '}') !== -1)
+      binding = binding.replace('{' + i + '}', args[i++]);
+
+    return execSync(binding);
+  }
+}());
